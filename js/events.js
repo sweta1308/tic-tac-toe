a13d26eeb5db44ba23b61045a3390a1aa2a44429
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2024-01-05 14:29:19 +0530

    feat: add events with listeners

---
+++ js/events.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", () => {
+  playerName.innerText = `${player}'s turn.`;
+  setKeys();
+  disableKeys();
+});
+restart.addEventListener("click", () => handleRestart());
+pieces.forEach((piece) => {
+  piece.addEventListener("dragstart", (e) => dragStart(e));
+});
+cells.forEach((cell) => {
+  cell.addEventListener("dragenter", dragEnter);
+  cell.addEventListener("dragover", dragOver);
+  cell.addEventListener("dragleave", dragLeave);
+  cell.addEventListener("drop", drop);
+});
